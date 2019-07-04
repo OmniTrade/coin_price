d4db034941f0cbf8acbfd10cc259c4186d75ec28
Elias Rodrigues <elias.rodrigues@usp.br>
2019-07-04 00:47:54 -0300

    Move Runner class to its own file

---
+++ lib/coin_price/refresher/runner.rb
@@ -0,0 +1,35 @@
+module CoinPrice
+  module Refresher
+    class Runner
+      def initialize(bases, quotes, source_id)
+        source = CoinPrice.find_source_klass(source_id)
+        @fetch = Fetch.new(bases, quotes, source, from_cache: false)
+      end
+
+      def run
+        loop do
+          puts 'Awake! Refreshing prices...'
+          @fetch.values
+          puts 'Done refreshing prices! Sleeping...'
+          sleep wait_seconds
+        rescue Interrupt => e
+          puts 'Stop refreshing prices.'
+          puts "#{e.class}: #{e.message}"
+          break
+        end
+      end
+
+      def wait_seconds
+        (Refresher.config.wait * multiplier).to_i
+      end
+
+      def multiplier(date = Time.now)
+        if date.saturday? || date.sunday?
+          Refresher.config.wait_weekend_multiplier
+        else
+          Refresher.config.wait_weekday_multiplier
+        end
+      end
+    end
+  end
+end
